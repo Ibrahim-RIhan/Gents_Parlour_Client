28c7888097a5eaefdf54d210513a8e56537aa9a1
rihan010 <ibrhimrihan000@gmail.comm>
2023-07-18 23:24:35 +0600

    Contact Form Done

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -1,11 +1,50 @@
-import React from 'react';
+import React from "react";
 
 const Contact = () => {
-    return (
-        <div>
-            Contact Us
+  return (
+    <div className="bg-[#FFF8F5] flex flex-col justify-center items-center h-full p-16">
+      <div>
+        <h3 className="text-5xl leading-relaxed font-bold text-center my-10">
+          Let us handle your <br />
+          Project Professionally
+        </h3>
+        <div className="text-center">
+          <input
+            className="border px-5 placeholder:text-gray-500 placeholder:px-1  border-gray-700 transition-all hover:scale-105 py-3 lg:mr-5 mb-5 rounded-lg "
+            placeholder="First Name"
+            type="text"
+          />
+          <input
+            className="border  placeholder:text-gray-500 placeholder:px-1 px-5 border-gray-700 transition-all hover:scale-105 py-3 rounded-lg "
+            placeholder="Last Name"
+            type="text"
+          />
         </div>
-    );
+        <div className="my-5 text-center">
+          <input
+            className="border  placeholder:text-gray-500 placeholder:px-1 border-gray-700 transition-all hover:scale-105 py-3 px-5 lg:mr-5  mb-5 rounded-lg "
+            placeholder="Email address"
+            type="email"
+          />
+          <input
+            className="border placeholder:text-gray-500 placeholder:px-1 px-5 border-gray-700 transition-all hover:scale-105 py-3 rounded-lg "
+            placeholder="Phone number"
+            type="text"
+          />
+        </div>
+        <div className="text-center ">
+          <textarea
+            placeholder="Your Message"
+            className=" placeholder:text-gray-500 hover:scale-105 placeholder:px-3 placeholder:py-2 w-full border border-gray-700 transition-all rounded-lg"
+            name=""
+            id=""
+            cols="20"
+            rows="7"
+          ></textarea>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,11 +1,50 @@
-
+import {
+  FaFacebook,
+  FaInstagram,
+  FaLocationArrow,
+  FaTwitter,
+  FaYoutube,
+} from "react-icons/fa";
+import{MdLocationOn} from "react-icons/md"
 
 const Footer = () => {
-    return (
+  return (
+    <footer 
+    className="grid grid-cols-1 md:grid-cols-2 py-16 px-9 bg-stone-800 text-gray-200 lg:grid-cols-4 items-start  justify-items-center">
+      <div className="flex items-start gap-5">
+        <MdLocationOn className="text-2xl"></MdLocationOn>
         <div>
-          Footer  
+          <p>H#000 (0th Floor), Road #00,</p>
+          <p>New DOHS, Mohakhali</p>
+          <p>Dhaka, Bangladesh</p>
         </div>
-    );
+      </div>
+      <div>
+        <h1 className="text-xl font-bold mb-3">Company</h1>
+        <p>About</p>
+        <p>Projects</p>
+        <p>Services</p>
+      </div>
+      <div>
+        <h1 className="text-xl font-bold mb-3">Quick Links</h1>
+        <p>About</p>
+        <p>Projects</p>
+        <p>Services</p>
+      </div>
+      <div>
+        <h3 className="text-xl font-bold mb-3">About Us</h3>
+        <p className="my-5">
+         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio, at.
+        </p>
+        <div className="flex gap-5 text-2xl">
+          <FaFacebook></FaFacebook>
+          <FaInstagram></FaInstagram>
+          <FaTwitter></FaTwitter>
+          <FaYoutube></FaYoutube>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
